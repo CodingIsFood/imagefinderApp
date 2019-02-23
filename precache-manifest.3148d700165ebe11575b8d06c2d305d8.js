9b6e665070a3017510b81816d7bf29d583a68b15
CodingIsFood <ayoodedere@gmail.com>
2019-02-24 00:15:26 +0100

    Updates

---
+++ precache-manifest.3148d700165ebe11575b8d06c2d305d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0043899c12053c7f9270",
+    "url": "/imagefinderApp/static/css/main.33e826d4.chunk.css"
+  },
+  {
+    "revision": "0043899c12053c7f9270",
+    "url": "/imagefinderApp/static/js/main.0043899c.chunk.js"
+  },
+  {
+    "revision": "834c282a6e2cebecef85",
+    "url": "/imagefinderApp/static/js/runtime~main.834c282a.js"
+  },
+  {
+    "revision": "1617acde5862d7885c5b",
+    "url": "/imagefinderApp/static/js/2.1617acde.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imagefinderApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a0ae1b4c0f43002585da34ffe082d4f",
+    "url": "/imagefinderApp/index.html"
+  }
+];
